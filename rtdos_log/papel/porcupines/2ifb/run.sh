c3e24f0f408d1e5ca7d1f1ff4c4dacd79e6759b6
pgbarletta <pbarletta@gmail.com>
2019-06-29 09:38:09 -0300

    Nuevos plots

--- rtdos_log/papel/porcupines/2ifb/run.sh
+++ rtdos_log/papel/porcupines/2ifb/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-./porky.jl -p avg_2ifb.pdb -v car_vgv_2ifb -o vgv_2ifb -m 30 --script
-./porky.jl -p avg_2ifb.pdb -v modes_2ifb -i 1 -o 1_2ifb -m -30 --script
-./porky.jl -p avg_2ifb.pdb -v modes_2ifb -i 2 -o 2_2ifb -m -30 --script
+#./porky.jl -p avg_2ifb.pdb -v car_vgv_2ifb -o vgv_2ifb -m 30 --script
+#./porky.jl -p avg_2ifb.pdb -v modes_2ifb -i 1 -o 1_2ifb -m -30 --script
+#./porky.jl -p avg_2ifb.pdb -v modes_2ifb -i 2 -o 2_2ifb -m -30 --script
+
+~/labo/ANA/build/ANA2 top_2ifb.pdb -c cut_beta.cfg -f cav_2ifb
